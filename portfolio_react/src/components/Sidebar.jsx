7a358091105f622b328cab4b89ab47e54097a1c6
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-11 16:02:37 -0300

    Ajustando arquivos Css

--- portfolio_react/src/components/Sidebar.jsx
+++ portfolio_react/src/components/Sidebar.jsx
@@ -17,18 +17,5 @@ const Sidebar = () => {
     )
 }
 
-const Sidebar = () => {
-    return (
-        <aside id="sidebar">
-            <img src={Avatar} alt="Perfil Danilo Duarte" />
-            <p className="title">Desenvolvedor</p>
-            <p>Redes Sociais</p>
-            <p>Informaçôes para contato</p>
-            <a href="#" className="btn">
-                Download Currículo
-            </a>
-        </aside>
-    )
-}
 
 export default Sidebar
