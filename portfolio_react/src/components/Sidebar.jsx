28f22c44f34b271894d5eee28f6556a321ec7882
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-12 09:23:02 -0300

    Componente gerado e importado ao app.jsx

--- portfolio_react/src/components/Sidebar.jsx
+++ portfolio_react/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Avatar from '../img/img-perfil.jpg';
 
 import "../styles/components/sidebar.css";
+import SocialNetWorks from './SocialNetWorks';
 
 
 const Sidebar = () => {
@@ -8,7 +9,7 @@ const Sidebar = () => {
         <aside id="sidebar">
             <img src={Avatar} alt="Perfil Danilo Duarte" />
             <p className="title">Desenvolvedor</p>
-            <p>Redes Sociais</p>
+            <SocialNetWorks />
             <p>Informaçôes para contato</p>
             <a href="#" className="btn">
                 Download Currículo
