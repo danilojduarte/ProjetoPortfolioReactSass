f1b0035d19979566939f32e71b9639dc082a7797
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-15 15:07:34 -0300

    Adicionado conteudo do #aboutContainer

--- portfolio_react/src/components/AboutContainer.jsx
+++ portfolio_react/src/components/AboutContainer.jsx
@@ -1,6 +1,9 @@
 const AboutContainer = () => {
     return (
-        <div>AboutContainer</div>
+        <section>
+            <h2>Sobre</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique fugiat natus dolore repellat eveniet, officiis aperiam? Rerum ea minima facilis aliquam modi voluptatum iure tempora similique, beatae illum possimus! Nemo.</p>
+        </section>
     );
 };
 
