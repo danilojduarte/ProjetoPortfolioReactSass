1c59b9deaf83ae4b6b7841fbf3d73b4258b63deb
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-17 16:03:08 -0300

    Iniciando estruturacao TecnologiesContainer

--- portfolio_react/src/components/AboutContainer.jsx
+++ portfolio_react/src/components/AboutContainer.jsx
@@ -1,6 +1,6 @@
 const AboutContainer = () => {
     return (
-        <section>
+        <section id="about-container">
             <h2>Sobre</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique fugiat natus dolore repellat eveniet, officiis aperiam? Rerum ea minima facilis aliquam modi voluptatum iure tempora similique, beatae illum possimus! Nemo.</p>
         </section>
--- portfolio_react/src/components/ProjetosContainer.jsx
+++ portfolio_react/src/components/ProjetosContainer.jsx
@@ -1,6 +1,16 @@
 const ProjetosContainer = () => {
     return (
-        <div>ProjetosContainer</div>
+        <section id="projetos-container">
+            <h2>Projetos</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eligendi tempora ad iure sunt, accusamus quo nesciunt eveniet voluptates totam consequatur, repudiandae deleniti debitis quas animi nemo. Optio, unde deserunt!
+            </p>
+
+            <a href="#" className="projetos-btn">
+                Visualizar Projetos
+            </a>
+
+        </section>
     );
 };
 
