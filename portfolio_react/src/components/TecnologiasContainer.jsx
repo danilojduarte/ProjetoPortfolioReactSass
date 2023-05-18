ec5453a4a27b4235c57bf722251d04b434b1033e
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-18 08:00:12 -0300

    Importando lib de icons

--- portfolio_react/src/components/TecnologiasContainer.jsx
+++ portfolio_react/src/components/TecnologiasContainer.jsx
@@ -1,3 +1,12 @@
+import { 
+    DiHtml5,
+    DiCss3,
+    DiJsBadge,
+    DiNodejsSmall,
+    DiMysql,
+    Direact
+} from 'react-icons/di';
+
 const TecnologiasContainer = () => {
     return (
         <div>TecnologiasContainer</div>
