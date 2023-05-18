1ebacdacf673ce0106955b625e81014f740e5804
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-18 08:09:52 -0300

    Libs importadas com sucesso

--- portfolio_react/src/components/TecnologiasContainer.jsx
+++ portfolio_react/src/components/TecnologiasContainer.jsx
@@ -4,8 +4,19 @@ import {
     DiJsBadge,
     DiNodejsSmall,
     DiMysql,
-    Direact
+    DiReact,
 } from 'react-icons/di';
+
+import "../styles/components/tecnologiascontainer.css";
+
+const tecnoligias = [
+    { id: "html", name: "HTML5", icon: <DiHtml5/> },
+    { id: "css", name: "CSS3", icon: <DiCss3/> },
+    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+    { id: "react", name: "React", icon: <DiReact /> },
+];
 
 const TecnologiasContainer = () => {
     return (
